5f031fd5edfdd33eb90fb216164ef16ad5505cb2
wildonion <ea_pain@yahoo.com>
2023-10-14 16:37:19 +0330

    change the repo to hadead

--- src/lib.rs
+++ src/lib.rs
@@ -35,14 +35,14 @@ impl Config{
         redis_port: &str, 
         chill_zone_duration_in_seconds: u64) -> Self{
 
-        let had_instance = serde_json::json!({
+        let hadead_instance = serde_json::json!({
             "redis_username": redis_username,
             "redis_host": redis_host,
             "redis_port": redis_port,
             "chill_zone_duration_in_seconds": chill_zone_duration_in_seconds
         });
 
-        let contract = generate_ed25519_contract(had_instance.as_str().unwrap());
+        let contract = generate_ed25519_contract(hadead_instance.as_str().unwrap());
         let id = contract.wallet.ed25519_public_key.clone().unwrap();
 
         Config { 
@@ -102,21 +102,21 @@ impl Config{
         let now = chrono::Local::now().timestamp_millis() as u64;
         let mut is_rate_limited = false;
         
-        let redis_result_had: RedisResult<String> = redis_conn.get(peer_unique_identifire).await;
-        let mut redis_had = match redis_result_had{
+        let redis_result_hadead: RedisResult<String> = redis_conn.get(peer_unique_identifire).await;
+        let mut redis_hadead = match redis_result_hadead{
             Ok(data) => {
                 let rl_data = serde_json::from_str::<HashMap<String, u64>>(data.as_str()).unwrap();
                 rl_data
             },
             Err(e) => {
-                let empty_had = HashMap::<String, u64>::new();
-                let rl_data = serde_json::to_string(&empty_had).unwrap();
+                let empty_hadead = HashMap::<String, u64>::new();
+                let rl_data = serde_json::to_string(&empty_hadead).unwrap();
                 let _: () = redis_conn.set(peer_unique_identifire, rl_data).await.unwrap();
                 HashMap::new()
             }
         };
         
-        if let Some(last_used) = redis_had.get(peer_unique_identifire){
+        if let Some(last_used) = redis_hadead.get(peer_unique_identifire){
             if now - *last_used < chill_zone_duration_in_seconds{
                 is_rate_limited = true;
             }
@@ -133,8 +133,8 @@ impl Config{
             
             /* updating the last rquest time */
             // this will be used to handle shared state between clusters
-            redis_had.insert(peer_unique_identifire.to_string(), now); //// updating the redis rate limiter map
-            let rl_data = serde_json::to_string(&redis_had).unwrap();
+            redis_hadead.insert(peer_unique_identifire.to_string(), now); //// updating the redis rate limiter map
+            let rl_data = serde_json::to_string(&redis_hadead).unwrap();
             let _: () = redis_conn.set(peer_unique_identifire, rl_data).await.unwrap(); //// writing to redis ram
 
             info!("🔓 Access Granted");
@@ -149,13 +149,13 @@ impl Config{
 }
 
 
-fn generate_ed25519_contract(had_instance: &str) -> wallexerr::Contract{
+fn generate_ed25519_contract(hadead_instance: &str) -> wallexerr::Contract{
 
     /* --------------------------------------- */
     /*          wallexerr operations           */
     /* --------------------------------------- */
     let mut data = DataBucket{
-        value: had_instance.to_string(), /* json stringify of config had instance */ 
+        value: hadead_instance.to_string(), /* json stringify of config had instance */ 
         signature: "".to_string(),
         signed_at: 0,
     };
